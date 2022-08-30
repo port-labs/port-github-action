40c8f30b2404f87564202c1690c72b8f41d680d2
Amit Shani <amit.shani1995@gmail.com>
2022-08-30 11:11:09 +0300

    set user agent on all requests

--- src/clients/port/index.ts
+++ src/clients/port/index.ts
@@ -1,6 +1,14 @@
+import axios from 'axios';
+
 import deleteEntity from './deleteEntity';
 import getToken from './getToken';
 import upsertEntity from './upsertEntity';
+
+const USER_AGENT = 'port-github-action/1.0.0';
+// wrap axios http methods with github-action user agent
+['get', 'post', 'delete', 'put'].forEach((method) => {
+	axios[method] = async (url: string, data?: any, config?: any) => axios[method](url, data, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
+});
 
 export default {
 	getToken,
