d62cbb8cff3d7c75f64b4461e754ea7c419728d2
Amit Shani <amit.shani1995@gmail.com>
2022-08-30 11:15:30 +0300

    explicit

--- src/clients/port/index.ts
+++ src/clients/port/index.ts
@@ -4,11 +4,16 @@ import deleteEntity from './deleteEntity';
 import getToken from './getToken';
 import upsertEntity from './upsertEntity';
 
-const USER_AGENT = 'port-github-action/1.0.0';
-// wrap axios http methods with github-action user agent
-['get', 'post', 'delete', 'put'].forEach((method) => {
-	axios[method] = async (url: string, data?: any, config?: any) => axios[method](url, data, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
-});
+const USER_AGENT = 'github-action/v1.0';
+const axget = axios.get;
+const axpost = axios.post;
+const axput = axios.put;
+const axdelete = axios.delete;
+
+axios.get = async (url: string, config?: any) => axget(url, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
+axios.post = async (url: string, data?: any, config?: any) => axpost(url, data, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
+axios.put = async (url: string, data?: any, config?: any) => axput(url, data, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
+axios.delete = async (url: string, config?: any) => axdelete(url, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
 
 export default {
 	getToken,
