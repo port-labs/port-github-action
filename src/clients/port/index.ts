4683a8e9ad1b8dc918773af111715462acbd5e7c
talsabagport <tal@getport.io>
2022-09-04 10:06:17 +0300

    Execute npm run all

--- src/clients/port/index.ts
+++ src/clients/port/index.ts
@@ -10,10 +10,14 @@ const axpost = axios.post;
 const axput = axios.put;
 const axdelete = axios.delete;
 
-axios.get = async (url: string, config?: any) => axget(url, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
-axios.post = async (url: string, data?: any, config?: any) => axpost(url, data, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
-axios.put = async (url: string, data?: any, config?: any) => axput(url, data, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
-axios.delete = async (url: string, config?: any) => axdelete(url, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
+axios.get = async (url: string, config?: any) =>
+	axget(url, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
+axios.post = async (url: string, data?: any, config?: any) =>
+	axpost(url, data, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
+axios.put = async (url: string, data?: any, config?: any) =>
+	axput(url, data, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
+axios.delete = async (url: string, config?: any) =>
+	axdelete(url, { ...config, headers: { ...config?.headers, 'User-Agent': USER_AGENT } });
 
 export default {
 	getToken,
